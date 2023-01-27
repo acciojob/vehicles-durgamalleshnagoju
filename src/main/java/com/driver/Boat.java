055eda9116823021867c2675f5aba4f5c928b3c7
Durga Mallesh Nagoju <nagojudurgamallesh@gmail.com>
2023-01-27 18:43:48 +0530

    Changes in Constructor of Boat

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -1,10 +1,11 @@
 package com.driver;
 
 public class Boat implements WaterVehicle{
-    String name;
-    int capacity;
-    Boat(){
-
+    private String name;
+    private int capacity;
+    Boat(String name, int capacity){
+        this.name = name;
+        this.capacity = capacity;
     }
 
     public String getVehicleName() {
